4274323051a33fc7a971497db36672f3d0876099
vaibhavigarje <garjevaibhavi261@gmail.com>
2025-07-17 17:52:20 +0530

    first change

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <div>
       <h1 className='bg-blue-300 text-center font-bold text-3xl underline'>Welcome...!</h1>
+      <br></br>
       <p className='bg-blue-300 text-center text-3xl'>This Is My First Project</p>
+
     </div>
   )
 }
