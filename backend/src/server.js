1f133370d36b0387c58727237b2ef9f4e093a9c6
Kartiki23 <kartikip2419@gmail.com>
2025-07-24 20:42:20 +0530

    Update server.js with latest changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import authRoutes from './route/DoctorRegistrationRoute.js';
 import { connectDB } from './config/Db.js';
+import docLoginRouter from './route/DoctorLoginRoute.js';
 
 
 const app = express()
+
 app.use(express.json())
+
 connectDB();
+
 const PORT = 3001
 
 app.use('/api/v1/user',authRoutes)
+
+app.use('/api/v1/user',docLoginRouter)
 
 app.use('/',(req,res)=> {
     res.send ("Hello World")
