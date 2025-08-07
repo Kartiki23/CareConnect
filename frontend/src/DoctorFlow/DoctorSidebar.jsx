025d75be42c10a5b5ecb69bf2464a9a06e169f65
Kartiki23 <kartikip2419@gmail.com>
2025-08-08 00:23:06 +0530

    Doctor Sidebar updated

--- frontend/src/DoctorFlow/DoctorSidebar.jsx
+++ frontend/src/DoctorFlow/DoctorSidebar.jsx
@@ -1,5 +1,6 @@
 import { CalendarIcon, FileText, LayoutDashboard, LogOut, MessageSquare, Pill, Settings, Sidebar, Users, Wallet } from 'lucide-react';
 import React from 'react';
+import { FaUserMd } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const DoctorSidebar = ({ children }) => {
@@ -13,16 +14,16 @@ const DoctorSidebar = ({ children }) => {
           <Link to ="/doctorAppointment"><div className="hover:text-indigo-600 cursor-pointer mt-2 flex "><CalendarIcon className="mr-2" />Appointments</div></Link>
           <Link to ="/patientDetails"><div className="hover:text-indigo-600 cursor-pointer mt-2 flex"> <Users className="mr-2" />Patients</div></Link>
           <Link to ="/doctormsg"><div className="hover:text-indigo-600 cursor-pointer flex mt-2"><MessageSquare className="mr-2" />Messages</div></Link>
-          <Link to ='/doctorProfile'><div className="hover:text-indigo-600 cursor-pointer flex mt-2"><Pill className="mr-2" />Your Profile</div></Link>
+          <Link to ='/doctorProfile'><div className="hover:text-indigo-600 cursor-pointer flex mt-2"><FaUserMd className="mr-2" />Your Profile</div></Link>
           <div className="hover:text-indigo-600 cursor-pointer flex mt-2"><FileText className="mr-2" />Documents</div>
-          <div className="hover:text-indigo-600 cursor-pointer flex mt-2"><Wallet className="mr-2" />Finances</div>
           <div className="hover:text-indigo-600 cursor-pointer flex mt-2"><Settings className="mr-2" />Settings</div>
-        </nav>
-        <Link to = "/login"><div className="absolute bottom-6">
-          <button className="bg-indigo-600 text-white mb-20 w-30 h-9 rounded-md hover:bg-indigo-700 flex">
+           <Link to = "/login"><div>
+          <button className="bg-indigo-600 text-white w-30 h-9 rounded-md hover:bg-indigo-700 flex">
             <LogOut className='mr-1 mt-1'/><h1 className='mt-1 font-bold text-lg'>Logout</h1>
           </button>
         </div></Link>
+        </nav>
+       
       </aside>
       {/* Main Content */}
       <main className="ml-64 flex-1 p-8 overflow-auto">
