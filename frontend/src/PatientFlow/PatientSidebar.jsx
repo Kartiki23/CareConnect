9e1567bac28f9c13c4f087576bf6f62e6fee06d5
Kartiki23 <kartikip2419@gmail.com>
2025-08-08 01:57:29 +0530

    Patient sidebar is updated

--- frontend/src/PatientFlow/PatientSidebar.jsx
+++ frontend/src/PatientFlow/PatientSidebar.jsx
@@ -1,5 +1,5 @@
 
-import { CalendarIcon, HeartPulse, MessageSquare, Sidebar } from 'lucide-react';
+import { CalendarIcon, HeartPulse, LogOut, MessageSquare, Sidebar } from 'lucide-react';
 import React from 'react';
 import { FaUserMd } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
@@ -11,26 +11,33 @@ const PatientSidebar = ({ children }) => {
       <aside className="w-64 bg-white shadow-md p-6">
          <div className="text-xl font-bold text-blue-600 mb-8">CareConnect</div> 
          <nav className="space-y-4"> 
-            <Link to ="/PatientDashboard"><div className="flex items-center text-blue-600 font-semibold">
-             <HeartPulse className="mr-2" /> Dashboard  
+            <Link to ="/PatientDashboard"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer mt-2">
+             <HeartPulse className="mr-2 " /> Dashboard  
              </div></Link>
             
-              <Link to ="/PatientAppointments"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer"> 
+              <Link to ="/PatientAppointments"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer mt-2"> 
                 <CalendarIcon className="mr-2" /> 
                 Appointments 
                 </div> 
                 </Link>
 
-                <Link to ="/patientmsg"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer"> 
+                <Link to ="/patientmsg"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer mt-2"> 
                 <MessageSquare className="mr-2" /> 
                 Message
                 </div> 
                 </Link>
 
-                <Link to ="/patientProfile"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer"> 
+                <Link to ="/patientProfile"><div className="flex items-center text-gray-600 hover:text-blue-600 cursor-pointer mt-2"> 
                 <FaUserMd className="mr-2" /> 
                 Your Profile
                 </div> 
+                </Link>
+
+                <Link to = "/login"><div>
+                  <button className="bg-indigo-600 text-white w-30 h-9 rounded-md hover:bg-indigo-700 flex mt-2">
+                  <LogOut className='mr-1 mt-1'/><h1 className='mt-1 font-bold text-lg'>Logout</h1>
+                  </button>
+                  </div>
                 </Link>
             </nav> 
         </aside>
