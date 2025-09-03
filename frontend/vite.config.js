16dfdda6cce79298bd50321e9243f6b01f96d8bf
Kartiki23 <kartikip2419@gmail.com>
2025-09-03 21:37:07 +0530

    viteconfig file updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins:  tailwindcss(),
-  base: "/CareConnect/", 
+  plugins: [tailwindcss()],  // ✅ must be inside []
+  base: "/CareConnect/",     // ✅ repo name, case-sensitive
 })
